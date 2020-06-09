be0f9b491c923012e372aaa8812f19328ac3a948
shenyy <936443267@qq.com>
2020-06-09 16:32:24 +0800

    first commit

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -0,0 +1,142 @@
+package com.leyou.item.controller;
+
+import com.leyou.item.pojo.Category;
+import com.leyou.item.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author: 98050
+ * Time: 2018-08-07 19:18
+ * Feature:
+ */
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 根据父节点查询商品类目
+     * @param pid
+     * @return
+     */
+    @GetMapping("/list")
+    public ResponseEntity<List<Category>> queryCategoryByPid(@RequestParam(value = "pid", defaultValue = "0") Long pid) {
+
+        try {
+            if (pid == null || pid.longValue() < 0){
+                // pid为null或者小于等于0，响应400
+                return ResponseEntity.badRequest().build();
+            }
+            // 执行查询操作
+            List<Category> categoryList = this.categoryService.queryCategoryByPid(pid);
+            if (CollectionUtils.isEmpty(categoryList)){
+                // 返回结果集为空，响应404
+                return ResponseEntity.notFound().build();
+            }
+            // 响应200
+            return ResponseEntity.ok(categoryList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 响应500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    /**
+     * 用于修改品牌信息时，商品分类信息的回显
+     * @param bid
+     * @return
+     */
+    @GetMapping("bid/{bid}")
+    public ResponseEntity<List<Category>> queryByBrandId(@PathVariable("bid") Long bid){
+        List<Category> list = this.categoryService.queryByBrandId(bid);
+        if(list == null || list.size() < 1){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(list);
+    }
+
+
+    /**
+     * 保存
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<Void> saveCategory(Category category){
+        this.categoryService.saveCategory(category);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 更新
+     * @return
+     */
+    @PutMapping
+    public ResponseEntity<Void> updateCategory(Category category){
+        this.categoryService.updateCategory(category);
+        return  ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    /**
+     * 删除
+     * @return
+     */
+    @DeleteMapping("cid/{cid}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable("cid") Long id){
+        this.categoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    /**
+     * 根据分类id集合查询分类名称
+     * @param ids
+     * @return
+     */
+    @GetMapping("names")
+    public ResponseEntity<List<String>> queryNameByIds(@RequestParam("ids")List<Long> ids){
+        List<String> list = categoryService.queryNameByIds(ids);
+        if (list == null || list.size() < 1){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }else {
+            return ResponseEntity.ok(list);
+        }
+    }
+
+    /**
+     * 根据分类id集合查询分类名称
+     * @param ids
+     * @return
+     */
+    @GetMapping("all")
+    public ResponseEntity<List<Category>> queryCategoryByIds(@RequestParam("ids")List<Long> ids){
+        List<Category> list = categoryService.queryCategoryByIds(ids);
+        if (list == null || list.size() < 1){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }else {
+            return ResponseEntity.ok(list);
+        }
+    }
+
+    /**
+     * 根据分类id集合查询分类名称
+     * @param id
+     * @return
+     */
+    @GetMapping("all/level/{cid3}")
+    public ResponseEntity<List<Category>> queryAllCategoryLevelByCid3(@PathVariable("cid3")Long id){
+        List<Category> list = categoryService.queryAllCategoryLevelByCid3(id);
+        if (list == null || list.size() < 1){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }else {
+            return ResponseEntity.ok(list);
+        }
+    }
+
+}
